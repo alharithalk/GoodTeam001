17cea622361c30198d53795b84d61464ba413097
abbhosni <binkhalidabdulahv@gmail.com>
2026-02-18 09:59:57 +0400

    Factorial Function added

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,19 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter the first number");
+        int num1 = input.nextInt();
+        System.out.println("Enter the 2nd number");
+        int num2 = input.nextInt();
 
+        //factorial function
+        int Function = 1;
+        for (int i = 1;i < num1;i++){
+            Function = Function * i;
         }
+        System.out.println("Factorial of " + num1 + " is :" + Function);
+
+
     }
+}
+
