8097c300a50c106d0ca4cd130ef0688388ae4242
alharithalk <kindaking99@gmail.com>
2026-02-18 10:11:09 +0400

    added my programm for absolute

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        // hk 99 Task one
+        System.out.print("enter  num: ");
+        int num1=input.nextInt();
+        int result= Math.abs(num1);
+
+        System.out.println("result = "+result);
+
+        input.close();
+
+
 
         }
     }
