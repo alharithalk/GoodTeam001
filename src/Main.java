a10d6fbd264146783b97eb55c15ff82bbeabef41
Your Name <you@example.com>
2026-02-18 10:03:57 +0400

    Odd and Even Numbers

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int inputNumber = scanner.nextInt();
 
+        // Calculation using modulus operator
+        if (inputNumber % 2 == 0) {
+            System.out.println("Result: Even");
+        } else {
+            System.out.println("Result: Odd");
         }
+
+        scanner.close();
     }
+}
